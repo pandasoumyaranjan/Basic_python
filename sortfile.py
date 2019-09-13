8fed398332a52dd871db5d112661a17bf89cdc7e
SOUMYARANJAN PANDA <41854901+pandasoumyaranjan@users.noreply.github.com>
2019-09-13 08:50:11 +0530

    Add files via upload

---
+++ sortfile.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+ 
+
+path = "C:\\Users\\user\\Downloads"
+
+list_ = os.listdir(path)
+ 
+
+for file_ in list_:
+    name,ext = os.path.splitext(file_)
+    
+    ext = ext[1:]
+   
+    if ext == '':
+        continue
+    
+    if os.path.exists(path+'/'+ext):
+       shutil.move(path+'/'+file_,path+'/'+ext+'/'+file_)
+    
+    else:
+        os.makedirs(path+'/'+ext)
+        shutil.move(path+'/'+file_,path+'/'+ext+'/'+file_)
